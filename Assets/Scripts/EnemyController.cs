c9a5c23ceac42e3757c09986a70f01e9af695e01
kagebushi <144465683+kagebushi@users.noreply.github.com>
2024-08-15 17:35:06 +0200

    Added sktbox, water, navMesh and Enemy skeleton

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform[] patrolPonts;
+    public int currentPatrolPoint;
+
+    public NavMeshAgent agent;
+    
+    public Animator animator;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(patrolPonts[currentPatrolPoint].position);
+
+        if (agent.remainingDistance <= .2f)
+        {
+            currentPatrolPoint++;
+            
+            if (currentPatrolPoint >= patrolPonts.Length)
+            {
+                currentPatrolPoint = 0;
+            }
+
+            agent.SetDestination(patrolPonts[currentPatrolPoint].position);
+        }
+
+        animator.SetBool("IsMoving", true);
+    }
+}
